637ac024b24fc02ec2d63af4e05ece2165b32a3c
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-05-30 23:55:19 +0700

    fix navigator

--- lib/main.dart
+++ lib/main.dart
@@ -67,8 +67,8 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute<void>(builder: (BuildContext context) {
       final tiles = _saved.map((WordPair pair) {
         return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
       });
